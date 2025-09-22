082a1540a2ce674115a2df94f88faa14c6ca60e9
Victoria Litvinova <vilit@microsoft.com>
2025-09-22 14:23:55 -0700

    prob should stop poking this for now

--- azext_edge/tests/edge/adr/test_namespace_asset_helpers_unit.py
+++ azext_edge/tests/edge/adr/test_namespace_asset_helpers_unit.py
@@ -172,6 +172,7 @@ def test_get_sub_property_error(property_key):
     expected_msg = f"{property_name} '{name}' not found in asset '{asset['name']}'."
     assert expected_msg in str(ex.value)
 
+
 @pytest.mark.parametrize("test_case", [
     # Test case 1: Move a single child successfully
     {
@@ -196,37 +197,105 @@ def test_get_sub_property_error(property_key):
     },
     # Test case 2: Move all children using wildcard
     {
-        "asset": {
-            "name": "testAsset",
-            "properties": {
-                "eventGroups": [
-                    {"name": "group1", "events": [{"name": "event1"}, {"name": "event2"}]},
-                    {"name": "group2", "events": []}
-                ]
-            }
+        "asset_properties": {
+            "parentKey": [
+                {"name": "group1", "events": [{"name": "event1"}, {"name": "event2"}]},
+                {"name": "group2", "events": []}
+            ]
         },
         "original_parent_name": "group1",
         "destination_parent_name": "group2",
         "child_names": ["*"],  # Wildcard
         "replace": False,
         "expected": {
-            "eventGroups": [
+            "parentKey": [
                 {"name": "group1", "events": []},
                 {"name": "group2", "events": [{"name": "event1"}, {"name": "event2"}]}
             ]
         }
-    }
+    },
+    # Test case 3: Move some children successfully
+    {
+        "asset_properties": {
+            "parentKey": [
+                {"name": "dataset1", "childKey": [
+                    {"name": "child1", "data": "data1"},
+                    {"name": "child2", "data": "data2"},
+                    {"name": "child3", "data": "data3"}
+                ]},
+                {"name": "dataset2", "childKey": []},
+                {"name": "dataset3", "childKey": []}
+            ]
+        },
+        "original_parent_name": "dataset1",
+        "destination_parent_name": "dataset2",
+        "child_names": ["child2", "child3"],
+        "replace": False,
+        "expected": {
+            "parentKey": [
+                {"name": "dataset1", "childKey": [
+                    {"name": "child1", "data": "data1"},
+                ]},
+                {"name": "dataset2", "childKey": [
+                    {"name": "child2", "data": "data2"},
+                    {"name": "child3", "data": "data3"}
+                ]},
+                {"name": "dataset3", "childKey": []}
+            ]
+        }
+    },
+    # Test case 4: Move children with replace
+    {
+        "asset_properties": {
+            "parentKey": [
+                {"name": "dataset1", "childKey": [
+                    {"name": "child1", "data": "data1"},
+                    {"name": "child2", "data": "data2"},
+                    {"name": "child3", "data": "data3"}
+                ]},
+                {"name": "dataset2", "childKey": [
+                    {"name": "child1", "data": "data5"},
+                    {"name": "child2", "data": "data6"},
+                    {"name": "child3", "data": "data7"}
+                ]},
+                {"name": "dataset3", "childKey": []}
+            ]
+        },
+        "original_parent_name": "dataset1",
+        "destination_parent_name": "dataset2",
+        "child_names": ["child2", "child3"],
+        "replace": True,
+        "expected": {
+            "parentKey": [
+                {"name": "dataset1", "childKey": [
+                    {"name": "child1", "data": "data1"}
+                ]},
+                {"name": "dataset2", "childKey": [
+                    {"name": "child1", "data": "data5"},
+                    {"name": "child2", "data": "data2"},
+                    {"name": "child3", "data": "data3"}
+                ]},
+                {"name": "dataset3", "childKey": []}
+            ]
+        }
+    },
 ])
 @pytest.mark.parametrize("parent_key, child_key", [
     ("datasets", "dataPoints"),
     ("eventGroups", "events"),
     ("managementGroups", "actions")
 ])
-def test_move_sub_props_success(test_case):
-    asset = test_case["asset"]
+def test_move_sub_props_success(test_case, parent_key, child_key):
+    # convert to a workable asset structure
+    asset_properties = test_case["asset_properties"]
+    asset_properties[parent_key] = asset_properties.pop("parentKey")
+    for child in asset_properties[parent_key]:
+        child[child_key] = child.pop("childKey")
+    asset = {"properties": test_case["asset_properties"]}
+
     _move_sub_props(
         asset,
-        parent_key=test_case["parent_key"],
+        parent_key=parent_key,
         original_parent_name=test_case["original_parent_name"],
         destination_parent_name=test_case["destination_parent_name"],
         child_names=test_case["child_names"],
